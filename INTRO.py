50b914830bedcfdfa2a24a21eae100482b1fec4c
Neha Bora <73404959+Maatu0477@users.noreply.github.com>
2020-10-25 16:24:30 +0530

    Introduction of Project

---
+++ INTRO.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 25 15:58:03 2020
+
+@author: D.K Bora
+"""
+
+#  Telecom-Call-Data-Record-Analysis
+
+
+
+CDR Analysis with Insights is a project basd on Data Analytis where we are given a platform to work with the client on real world dataset.
+
+We need to visualize the Analytics for Call Data, Device Data and Service Data and need to develop a POC for one of itsesteemed clients for Unified Cll Daata Record Analysis
+
+This work requires us to do Data Prparation and required us to find Data insights from raw CDR data provided to us by the client; including Data Cleaning and need to show important informations like the cards, pie chart, tabular data, etc.
+
+This is an emerging and a growing field.
